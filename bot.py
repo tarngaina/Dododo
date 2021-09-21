fd8c26edb37a068812a303635c985f84722025f0
Gia An <46303685+tarngaina@users.noreply.github.com>
2021-09-21 11:55:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -217,6 +217,7 @@ async def _play(ctx, *, text):
         await ctx.send(embed = embed)
       else:
         res2, url = youtube.search(text, single = True)
+        print(url)
         if res2:
           res, songs = await youtube.get_info(url)
           songs = [songs]
