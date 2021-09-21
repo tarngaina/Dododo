ee2b90496ddc86c1461226d879dd3b65817b0a39
Gia An <46303685+tarngaina@users.noreply.github.com>
2021-09-21 08:40:53 +0700

    Create util.py

---
+++ util.py
@@ -0,0 +1,40 @@
+from os import path, makedirs
+from json import dump, load
+from random import randint
+from discord import Color
+
+def to_int(obj):
+  try:
+    return True, int(obj)
+  except:
+    return False, obj
+  
+def to_float(obj):
+  try:
+    return True, float(obj)
+  except:
+    return False, obj
+  
+def random_color():
+  return Color.from_rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+
+
+def strip_ansi(s):
+  return compile(r'\x1B\[\d+(;\d+){0,2}m').sub('', s)
+
+def save_prefs(id, prefs):
+  if len(prefs) > 0:
+    with open(f'data/{id}.json', 'w+', encoding = 'utf-8') as f:
+      dump(prefs, f, ensure_ascii = False, indent = 2)
+    
+def load_prefs(id):
+  if path.exists(f'data/{id}.json'):
+    with open(f'data/{id}.json', 'r', encoding = 'utf-8') as f:
+      return load(f)
+  else:
+    with open(f'data/{id}.json', 'w+', encoding = 'utf-8') as f:
+      dump({}, f, indent = 2)
+    return {}
+
+if not path.exists('data'):
+  makedirs('data')
