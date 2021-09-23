493f2db3c086876b7a3080e8301cf8693dbbb79a
Gia An <46303685+tarngaina@users.noreply.github.com>
2021-09-23 17:29:37 +0700

    Create reminder.py

---
+++ reminder.py
@@ -0,0 +1,20 @@
+OWNER_ID = 0
+owner = None
+
+CHANNEL_ID = 890542648769806347
+channel = None
+
+def data_prepare(bot):
+  global owner, channel
+  owner = bot.get_user(OWNER_ID)
+  channel = bot.get_channel(CHANNEL_ID)
+  print(owner.name, channel.name)
+    
+async def send():
+  global
+  if not channel:
+    return
+  if not owner:
+    return
+  
+  await channel.send(f'{owner.mention} deploy.')
