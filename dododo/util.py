9b5e2a751d232d35d02863940b14e382f7f105d4
Gia An <46303685+tarngaina@users.noreply.github.com>
2021-10-24 01:05:49 +0700

    Add files via upload

---
+++ dododo/util.py
@@ -0,0 +1,48 @@
+from random import randint
+from datetime import datetime
+
+from discord import Color
+from pytz import timezone
+
+def to_int(obj):
+  try:
+    return True, int(obj)
+  except:
+    return False, obj
+
+def now():
+  return datetime.now(timezone("Asia/Ho_Chi_Minh"))
+
+def now_str():
+  return now().strftime('%H:%M - %A, %B %d, %Y ')
+  
+def random_color():
+  return Color.from_rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+
+
+class Page():
+  pages = []
+  
+  @staticmethod
+  def get_page(id):
+    for p in Page.pages:
+      if p.message.id == id:
+        return p
+    return None
+
+  def __init__(self, message = None, value = 1):
+    self.message = message
+    self.value = value
+
+  def set(self, value):
+    self.value = value
+
+  def increase(self, max_page, value = 1):
+    self.value += value
+    if self.value > max_page:
+      self.value = max_page
+
+  def decrease(self, max_page, value = 1):
+    self.value -= value
+    if self.value < 1:
+      self.value = 1
