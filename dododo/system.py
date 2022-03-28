d2325c280f5237216a33aa2ecfb1998101848c74
Gia An <46303685+tarngaina@users.noreply.github.com>
2022-03-28 10:00:08 +0700

    Update system.py

--- dododo/system.py
+++ dododo/system.py
@@ -53,7 +53,7 @@ async def update():
       await restart()
 
 
-@tasks.loop(seconds = 300)
+@tasks.loop(seconds = 600)
 async def send_discord_log():
   if not log_channel2:
     print('Warning: No log2 channel found.')
@@ -66,7 +66,7 @@ async def send_discord_log():
 def start_discord_log():
   logger = logging.getLogger('discord')
   logger.setLevel(logging.DEBUG)
-  handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+  handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
   handler.setFormatter(logging.Formatter('[%(asctime)s]:[%(levelname)s]:[%(name)s]: %(message)s'))
   logger.addHandler(handler)
   send_discord_log.start()
